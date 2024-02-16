bd68f0b1d1d4261a1bbf2da6f5f7bec4460178a0
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-02-16 08:55:58 -0700

    schema in models is added but need defaults

--- models/flight.js
+++ models/flight.js
@@ -2,7 +2,7 @@
 // is to create and export our object Model (which we use in our controllers)
 // The model performs cruds operations on the movies collection in our movies database!
 const mongoose = require('mongoose')
-
+//optional shortcut to mongoose.Shema class
 const Schema = mongoose.Schema;
 
 // Enforces the shape of the documents (Think of objects)
@@ -14,14 +14,23 @@ const flightSchema = new Schema({
   },
   airport: {
     type: String,
+    //need to learn how to add DEN as a default
     enum: ['AUS','DFW','DEN','LAX','SAN']
   },
-  flightNo: 
-  nowShowing: Boolean
-}, {
-  timestamps: true
-});
-	
+  flightNo: {
+  type: Number,
+  min: 10,
+  max: 9999
+  },
+  //how to add default one year from today 
+  departs: {
+    type: Date,
+},
+// {
+//     timestamps: true
+// }
+})
+
 // Compile the schema into a model and export it
 // Movie, creates a movies collection in our movies database
 module.exports = mongoose.model('Flight', flightSchema);

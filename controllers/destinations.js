737ba10ceb0ae2afdab8d738b417b74c96dd48e0
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-02-21 12:29:28 -0700

    completed sans bonus

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -8,13 +8,11 @@ async function create(req, res){
     //to find the flight
     try {
         //req.params.id comes from the http request from the reviews form on the fllights index page
-        const flightDoc = await FlightModel.findById(req.params.id)
+        const flightDoc = await FlightModel.findById(req.params.id);  
         //flightDocs is the flight from the database--CHECK ON THIS IF ERROR IS THROWN
-        //then add the destination to the flight's array
+        //then add the destination to the flight's array      
         flightDoc.destinations.push(req.body);
         // Sort the destinations array
-        flightDoc.destinations.sort({arrival: 1});
-
         //to the destinations array
         //since we are changing the flightDocs we have to tell the database
         await flightDoc.save() //this tells the db we need to add the destination
